f5f34749a578b9685af7d4200b667735f3522efa
cfg0523 <cfg0523@sina.com>
2016-06-22 15:20:47 +0800

    checkin test

--- src/main/java/com/techsen/maven/plugins/version/StudyMojo.java
+++ src/main/java/com/techsen/maven/plugins/version/StudyMojo.java
@@ -7,6 +7,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.scm.ScmException;
 import org.apache.maven.scm.command.checkin.CheckInScmResult;
 import org.apache.maven.scm.command.status.StatusScmResult;
+import org.apache.maven.scm.command.tag.TagScmResult;
 
 @Mojo(name = "study")
 public class StudyMojo extends AbstractScmMojo {
@@ -17,9 +18,7 @@ public class StudyMojo extends AbstractScmMojo {
 
         try {
             StatusScmResult ssr = this.getScmManager().status(this.getScmRepository(), this.getFileSet());
-
             this.checkResult(ssr);
-
             System.out.println("******status start********");
             System.out.println(ssr.getCommandLine());
             System.out.println(ssr.getChangedFiles());
@@ -27,14 +26,17 @@ public class StudyMojo extends AbstractScmMojo {
             
             CheckInScmResult csr = this.getScmManager().checkIn(this.getScmRepository(), this.getFileSet(), "checkin test");
             this.checkResult(csr);
-            
             System.out.println("******checkIn start*******");
             System.out.println(csr.getCommandLine());
-            System.out.println(csr.getCommandOutput());
-            System.out.println(csr.getProviderMessage());
-            System.out.println(csr.getScmRevision());
             System.out.println(csr.getCheckedInFiles());
             System.out.println("******checkIn start*******");
+            
+            TagScmResult tsr = this.getScmManager().tag(this.getScmRepository(), this.getFileSet(), "1.0", "tag test");
+            this.checkResult(tsr);
+            System.out.println("*****tag start************");
+            System.out.println(tsr.getCommandLine());
+            System.out.println(tsr.getTaggedFiles());
+            System.out.println("*****tag end**************");
 
         } catch (ScmException | IOException e) {
             e.printStackTrace();
